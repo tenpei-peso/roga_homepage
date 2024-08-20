ef5b0ea413a7039bb02af3fd6a8a4bf317fba9b8
tenpei-sano <kikichan.fps@gmail.com>
2024-08-20 19:10:48 +0900

    commit

--- src/app/discipline/page.js
+++ src/app/discipline/page.js
@@ -24,35 +24,15 @@ export default function DisciplinePage() {
 
       <div className="flex justify-around my-8">
         {/* カード */}
-        <Card
-          isFooterBlurred
-          className="w-full h-[300px] col-span-12 sm:col-span-7"
-        >
-          <CardHeader className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Services
-            </p>
-            <h4 className="text-white/90 font-medium text-xl">
-              出張訓練＆幼稚園通園コース
-            </h4>
-          </CardHeader>
+        
           <Image
             width={600}
             height={300}
             alt="Relaxing app background"
-            className="z-0 w-full h-full object-cover"
+            className="w-3/4 h-full object-cover"
             src="/situke.jpeg"
           />
-          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-            <div className="flex flex-grow gap-2 items-center">
-              <div className="flex flex-col">
-                <p className="text-tiny text-white/60">
-                  出張訓練＆幼稚園通園コース
-                </p>
-              </div>
-            </div>
-          </CardFooter>
-        </Card>
+          
         {/* 説明 */}
       </div>
       <Spacer y={4} />
