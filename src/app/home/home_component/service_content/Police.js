68e7f83ae07f9be5f60d738c3034b074f7233d1e
tenpei-sano <kikichan.fps@gmail.com>
2024-08-20 19:00:14 +0900

    commit

--- src/app/home/home_component/service_content/Police.js
+++ src/app/home/home_component/service_content/Police.js
@@ -14,34 +14,15 @@ export default function Entrance() {
       </div>
 
       <div className="flex justify-around my-8">
-        {/* カード */}
-        <Card
-          isFooterBlurred
-          className="w-full h-[300px] col-span-12 sm:col-span-7"
-        >
-          {/* <CardHeader className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              Services
-            </p>
-            <h4 className="text-white/90 font-medium text-xl">
-              競技会・警察犬訓練
-            </h4>
-          </CardHeader> */}
+        
           <Image
-            width={600}
+            width={400}
             height={300}
             alt="Relaxing app background"
-            className="w-full h-full object-cover"
+            className="w-3/4 h-full object-cover"
             src="/police.jpeg"
           />
-          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-            <div className="flex flex-grow gap-2 items-center">
-              <div className="flex flex-col">
-                <p className="text-tiny text-white/60">競技会・警察犬訓練</p>
-              </div>
-            </div>
-          </CardFooter>
-        </Card>
+          
         {/* 説明 */}
       </div>
       <Spacer y={6} />
